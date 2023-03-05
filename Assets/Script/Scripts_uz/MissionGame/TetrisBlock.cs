189b07dd3f469fa7a0d9a0de14d4e42493fa56ce
YuJung01 <124559065+YuJung01@users.noreply.github.com>
2023-03-05 19:32:42 +0900

    .
    .

---
+++ Assets/Script/Scripts_uz/MissionGame/TetrisBlock.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisBlock : MonoBehaviour
+{
+    private float previousTime;
+    public float fallTime = 1.0f;
+    public static int height = 20;
+    public static int width = 10;
+    public Vector3 rotationPoint;
+    private static Transform[,] grid = new Transform[width, height];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(-1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+            if (!ValidMove())
+            {
+                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+            }
+        }
+
+        if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime/10 : fallTime))
+        {
+            transform.position += new Vector3(0, -1, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+                AddToGrid();
+                this.enabled = false;                                   // 블록이 바닥에 닿으면 새로운 블럭 생성
+                FindObjectOfType<spawnertetris>().NewTetris();
+            }
+            previousTime = Time.time;
+        }
+
+    }
+
+    bool HasLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (grid[j, i] == null)
+                return false;
+        }
+        return true;
+    }
+
+    void DeleteLine(int i)
+    {
+        for(int j=0; j<width; j++)
+        {
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+        }
+    }
+
+    void RowDown(int i)
+    {
+        for(int y=i; y<height; y++)
+        {
+            for(int j=0; j<width; j++)
+            {
+                if (grid[j, y] != null)
+                {
+                    grid[j, y - 1] = grid[j, y];
+                    grid[j, y] = null;
+                    grid[j, y - 1].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
+        }
+    }
+
+    void CheckForLines()
+    {
+        for(int i=height-1; i>=0; i--)
+        {
+            if (HasLine(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+            }
+        }
+    }
+
+    void AddToGrid()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundX = Mathf.RoundToInt(children.transform.position.x);
+            int roundY = Mathf.RoundToInt(children.transform.position.y);
+
+            grid[roundX, roundY] = children;
+        }
+    }
+
+    bool ValidMove()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundX = Mathf.RoundToInt(children.transform.position.x);
+            int roundY = Mathf.RoundToInt(children.transform.position.y);
+
+            if (roundX < 0 || roundX >= width || roundY < 0 || roundY >= height)
+            {
+                return false;
+            }
+
+            if (grid[roundX, roundY] != null)
+                return false;
+        }
+        return true;
+    }
+    
+}
---
+++ Assets/Script/Scripts_uz/MissionGame/Tile.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (spriteRenderer == null)
+        {
+            Debug.LogError("You need to SpriteRenderer for Block");
+        }
+    }
+    SpriteRenderer spriteRenderer;
+
+    public Color color
+    {
+        set
+        {
+            spriteRenderer.color = value;
+        }
+
+        get
+        {
+            return spriteRenderer.color;
+        }
+    }
+
+    public int sortingOrder
+    {
+        set
+        {
+            spriteRenderer.sortingOrder = value;
+        }
+
+        get
+        {
+            return spriteRenderer.sortingOrder;
+        }
+    }
+}
