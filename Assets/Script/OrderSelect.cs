bb9f27ecd72045e173e51f965c5da4a74b9336d1
조은채 <83952073+whdmsco010@users.noreply.github.com>
2023-03-08 13:47:14 +0900

    minigame1,2

---
+++ Assets/Script/OrderSelect.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderSelect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+/*
+public void OnClickFire()
+{
+    isfirst = true
+    //중요 자료를 획득하면 불타는 자료를 볼 수 있다.
+    if (isfirst)
+    {
+        SceneManager.LoadScene("CH3_fire");
+    }
+}
+public void OnClickDoor()
+{
+    //자료가 불타면 문으로 나갈 수 있다.
+    if (isFired)
+        SceneManager.LoadScene("CH3_door");
+}*/
